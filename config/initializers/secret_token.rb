cad1b99e305140269e99622df99ef4fcd58c0ff1
Matt Scilipoti <matt@scilipoti.name>
2012-01-29 10:28:45 -0500

    rails new trading_room

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TradingRoom::Application.config.secret_token = 'e30405111950911d50db39d43e82aaee10b45ac6b307084566338fe09791f0afa29c6b42b83cc3544aeef449abfd61139824b68863ee883bc9fae79752a1b959'
